322897be1f3625f166baed5d386c5a053cac4da0
Andréa J S França <dejandrea@gmail.com>
2022-03-28 09:18:32 -0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import HelloWorld from './Hello';
+
+function App() {
+  return (
+    <div>
+      <HelloWorld />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Hello.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+//componente function
+/*function HelloWorld(){
+    return(
+      <h1>Hello World</h1>
+    )
+}*/
+
+//componente classe
+class HelloWorld extends React.Component{
+    render(){
+        return(
+            <h1>Hello World</h1>
+        )
+    }
+}
+
+export default HelloWorld
